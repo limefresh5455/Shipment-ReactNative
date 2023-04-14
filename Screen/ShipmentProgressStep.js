29ff01048c4d7dbae0efc21e73a8a192f538aaaf
linuxbean001 <linuxbean001>
2023-04-14 18:10:15 +0530

    updates

--- Screen/ShipmentProgressStep.js
+++ Screen/ShipmentProgressStep.js
@@ -25,7 +25,7 @@ import { CheckBox, Stack } from "@rneui/themed";
 import OrderDetails from "./OrderDetails";
 // import PaymentModal from "./PaymentModal";
 import { Dialog } from "@rneui/themed";
-import { CardForm, StripeProvider } from "@stripe/stripe-react-native";
+import { CardField, StripeProvider } from "@stripe/stripe-react-native";
 import { Formik } from "formik";
 import * as Yup from "yup";
 
@@ -391,16 +391,25 @@ export default function ShipmentProgressStep({ navigation }) {
                           <Text style={styles.error}>{errors.holderName}</Text>
                         )}
 
-                        <CardForm
-                          postalCodeEnabled={false}
-                          onFormComplete={(cardDetails) => {
-                            setCardDetails(cardDetails);
+                        <CardField
+                          postalCodeEnabled={true}
+                          placeholders={{
+                            number: '4242 4242 4242 4242',
+                          }}
+                          cardStyle={{
+                            backgroundColor: '#FFFFFF',
+                            textColor: '#000000',
                           }}
                           style={{
-                            height: 200,
-                            justifyContent: "center",
-                            alignItems: "center",
-                            textAlign: "center",
+                            width: '100%',
+                            height: 50,
+                            marginVertical: 30,
+                          }}
+                          onCardChange={(cardDetails) => {
+                            console.log('cardDetails', cardDetails);
+                          }}
+                          onFocus={(focusedField) => {
+                            console.log('focusField', focusedField);
                           }}
                         />
 
